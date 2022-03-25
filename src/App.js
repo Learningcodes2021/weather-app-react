3a45efcda6ef4e46a06945cb49f171c358744147
Learningcodes2021 <91432796+Learningcodes2021@users.noreply.github.com>
2022-03-25 17:45:28 -0400

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-Import React from "react";
+import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App-React</div>;
+  return (
+    <div className="App">
+      <h1>Weather App React</h1>
+      <footer>
+        This project was coded by Jacqueline Berry and is{" "}
+        <a
+          href="https://github.com/Learningcodes2021/weather-app-react"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-
