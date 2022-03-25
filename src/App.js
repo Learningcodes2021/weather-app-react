8cad5a6c9aafc54a0cfb0ce76f15092075124f79
Learningcodes2021 <91432796+Learningcodes2021@users.noreply.github.com>
2022-03-25 19:26:46 -0400

    Added bootstrap to my project

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App React</h1>
-      <footer>
-        This project was coded by Jacqueline Berry and is{" "}
-        <a
-          href="https://github.com/Learningcodes2021/weather-app-react"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div class="container">
+        <h1>Weather App React</h1>
+        <footer>
+          This project was coded by Jacqueline Berry and is{" "}
+          <a
+            href="https://github.com/Learningcodes2021/weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
