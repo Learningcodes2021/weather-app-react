dfb548cd982feeb46bd4ac063557544ec30311c3
Learningcodes2021 <91432796+Learningcodes2021@users.noreply.github.com>
2022-03-30 09:10:35 -0400

    Repositioned temperature information

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -12,10 +12,6 @@ export default function WeatherTemperature(props) {
     setUnit("celsius");
   }
 
-  function fahrenheit() {
-    return (props.celsius * 9) / 5 + 32;
-  }
-
   if (unit === "celsius") {
     return (
       <div className="WeatherTemperature">
@@ -29,6 +25,7 @@ export default function WeatherTemperature(props) {
       </div>
     );
   } else {
+    let fahrenheit = (props.celsius * 9) / 5 + 32;
     return (
       <div className="WeatherTemperature">
         <span className="temperature">{Math.round(fahrenheit)}</span>
