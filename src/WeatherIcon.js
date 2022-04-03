1035e5669bf62f1e93a35c11a82a54566f854774
Learningcodes2021 <91432796+Learningcodes2021@users.noreply.github.com>
2022-04-03 08:13:02 -0400

    Refactored code and fixed warnings

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,8 +26,8 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="1e1e1e"
-      size={52}
+      color="#00d4ff"
+      size={props.size}
       animate={true}
     />
   );
